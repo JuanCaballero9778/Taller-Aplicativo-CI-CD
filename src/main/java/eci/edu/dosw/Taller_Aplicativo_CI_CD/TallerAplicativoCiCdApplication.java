32facb559128a9498eb32a7501d5db6bbcb862a4
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-10-19 19:50:49 -0500

    feature - Estructura inicial

---
+++ src/main/java/eci/edu/dosw/Taller_Aplicativo_CI_CD/TallerAplicativoCiCdApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.Taller_Aplicativo_CI_CD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerAplicativoCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerAplicativoCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/dosw/Taller_Aplicativo_CI_CD/TallerAplicativoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.Taller_Aplicativo_CI_CD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerAplicativoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
